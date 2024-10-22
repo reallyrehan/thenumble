a5051f3bc402141a316f5fc231acc9f9f1a11390
Samuel Kuhn <Samuel.kuhn@gmx.ch>
2024-10-22 20:30:29 +0200

    refactoring utils.py

--- utils.py
+++ utils.py
@@ -4,38 +4,29 @@ import re
 
 def new_equation_generator(seed):
     random.seed(int(seed))
-
-    op_list = ['+','-','/','*']
-
-    # ans_st = ""
+    op_list = ['+', '-', '/', '*']
     retry = True
-    # ans_ls = []
 
     while True:
-    #     input()
         ans_st = ""
         ans_ls = []
-        while True:
 
-            
-            
+        while True:
             a = (random.randint(0,9))
             b = (op_list[random.randint(0,3)])
-
-            
             ans_ls.append(a)
             ans_st = ans_st + str(a) +b
 
-            if len(ans_st)>7:
+            if len(ans_st) > 7:
                 break
+
         final_true = (ans_st[0:7])
-    #     print(final_true)
 
         try:
             eval_result = eval(final_true)
-    #         print(eval_result)
-            if eval_result == int(eval_result) and abs(eval_result)<200  and not re.search('(0\*)|(\*0)|(/0)|(0/)',final_true):
+            if eval_result == int(eval_result) and abs(eval_result)<200  and not re.search('(0\*)|(\*0)|(/0)|(0/)', final_true):
                 retry = False
+
         except:
             pass
 
@@ -44,4 +35,4 @@ def new_equation_generator(seed):
         else:
             break
     
-    return final_true,eval_result
+    return final_true, eval_result
