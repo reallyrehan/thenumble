55fe802afe9c3da29dd2574b3ced1310ddd8d189
Rehan Ahmed <33403815+reallyrehan@users.noreply.github.com>
2025-03-08 01:33:45 +0100

    hotfix: added division whole number check to ensure division operations don't have decimal results (#12)
    * hotfix: added division whole number check to ensure division operations don't have decimal results

--- utils.py
+++ utils.py
@@ -21,12 +21,25 @@ def new_equation_generator(seed):
                 break
 
         final_true = (ans_st[0:7])
+        
 
         try:
             eval_result = eval(final_true)
             if eval_result == int(eval_result) and abs(eval_result)<200  and not re.search('(0\*)|(\*0)|(/0)|(0/)', final_true):
                 retry = False
 
+            # check if the division is a whole number
+            for i in range(0, len(final_true)):
+                if final_true[i] == '/':
+                    before_div = final_true[i-1]
+                    after_div = final_true[i+1]
+
+                    val = float(int(before_div)/int(after_div))
+                    round_val = float(round(val))
+
+                    if val != round_val:
+                        retry = True
+                        break
         except:
             pass
 
